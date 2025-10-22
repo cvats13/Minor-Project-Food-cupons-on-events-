d69c7a2642a2c109992aaa3951184ca1236a5bac
chhavi <cvats9560@gmail.com>
2025-10-22 23:08:37 +0530

    Co-authored-by: Hardik Dhawan <HardikDhawan9311@users.noreply.github.com> Co-authored-by: VVanshikaaa <VVanshikaaa@users.noreply.github.com>

--- Client/Food-Cupon/src/App.jsx
+++ Client/Food-Cupon/src/App.jsx
@@ -1,6 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./Components/Auth/signup";
 import SignIn from "./Components/Auth/signin";
+import Home from "./Components/home/Home";
+
+
+
+
 
 
 function App() {
@@ -9,6 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Signup />} />
         <Route path="/signin" element={<SignIn />} />
+        <Route path="/Home" element={<Home/>} />
+         
       </Routes>
     </Router>
   );
